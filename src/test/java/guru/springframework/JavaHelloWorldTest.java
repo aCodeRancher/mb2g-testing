bfa538b008781a059b266bcf9e93dcfc20160150
helen <hhma@alumni.cpp.edu>
2021-01-17 20:26:13 -0500

    add Junt4 test

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,10 +1,22 @@
 package guru.springframework;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class JavaHelloWorldTest {
 
+    JavaHelloWorld jhw ;
+
+    @Before
+    public void setUp(){
+        jhw= new JavaHelloWorld();
+    }
+
     @Test
     public void getHello() {
+       String output = jhw.getHello();
+       assertTrue(output.equals("Hello World"));
     }
 }
