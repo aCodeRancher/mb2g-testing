ef2a93cee52abad1c20a25c56947ccb948bedab0
helen <hhma@alumni.cpp.edu>
2021-01-17 21:09:56 -0500

    add details to the junit 4 and junit 5 tests

--- src/test/java/guru/springframework/JUnit5Test.java
+++ src/test/java/guru/springframework/JUnit5Test.java
@@ -1,13 +1,23 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jt on 2018-12-12.
  */
 public class JUnit5Test {
 
+    JavaHelloWorld jhw;
+    @BeforeEach
+    public void setUp(){
+        jhw = new JavaHelloWorld();
+    }
     @Test
     void exampleTest() {
+        String output = jhw.getHello();
+        assertTrue(output.equals("Hello World"));
     }
 }
--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,10 +1,18 @@
 package guru.springframework;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JavaHelloWorldTest {
 
+    JavaHelloWorld jhw;
+    @Before
+    public void setUp(){
+        jhw = new JavaHelloWorld() ;
+    }
     @Test
     public void getHello() {
+        String output = jhw.getHello();
+        assert(output.equals("Hello World"));
     }
 }
